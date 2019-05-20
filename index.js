2279c6c9882dc35d756ce55f5a70efb17eb4b3fd
Flor Zarate <florz@zoomcatalog.com>
2019-05-20 14:32:02 -0500

    Modulo sencillo con saludos, creado

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log('Hola mundo');
+    },
+    holaCustom: function(name) {
+        console.log(`Hola ${name}`);
+    },
+    holaAlCursoNPM: function() {
+        console.log('Hola al curso NPM');
+    }
+}
